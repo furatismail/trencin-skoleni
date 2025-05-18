8b38e610c22e07cabfeec5ce80ca0089988db25c
Furát Ismail <furat.ismail@cadcom.cz>
2025-05-18 20:34:22 +0200

    refactor

--- src/app/components/desserts/desserts.component.ts
+++ src/app/components/desserts/desserts.component.ts
@@ -1,24 +1,27 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { DessertService } from '../../services/dessert.service';
 import { RatingService } from '../../services/rating.service';
 import { Dessert, DessertFilter, DessertIdToRatingMap } from '../../models/dessert.model';
 import { FormsModule } from '@angular/forms'
 import { NgFor, NgIf } from '@angular/common';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-desserts',
   imports: [FormsModule, NgIf, NgFor],
   templateUrl: './desserts.component.html',
   styleUrl: './desserts.component.css'
 })
-export class DessertsComponent implements OnInit {
-  #dessertService = inject(DessertService);
-  #ratingService = inject(RatingService);
+export class DessertsComponent implements OnInit, OnDestroy {
 
   originalName = '';
   englishName = '';
   loading = false;
 
   desserts: Dessert[] = [];
+
+  private readonly dessertService = inject(DessertService);
+  private readonly ratingService = inject(RatingService);
+  private readonly subscription = new Subscription();
 
   ngOnInit(): void {
     this.search();
@@ -32,7 +35,7 @@ export class DessertsComponent implements OnInit {
 
     this.loading = true;
 
-    this.#dessertService.find(filter).subscribe({
+    this.subscription.add(this.dessertService.find(filter).subscribe({
       next: (desserts) => {
         this.desserts = desserts;
         this.loading = false;
@@ -41,7 +44,7 @@ export class DessertsComponent implements OnInit {
         console.error('Search error', error);
         this.loading = false;
       },
-    });
+    }));
   }
 
   toRated(desserts: Dessert[], ratings: DessertIdToRatingMap): Dessert[] {
@@ -53,7 +56,7 @@ export class DessertsComponent implements OnInit {
   loadRatings(): void {
     this.loading = true;
 
-    this.#ratingService.loadExpertRatings().subscribe({
+    this.subscription.add(this.ratingService.loadExpertRatings().subscribe({
       next: (ratings) => {
         const rated = this.toRated(this.desserts, ratings);
         this.desserts = rated;
@@ -63,6 +66,10 @@ export class DessertsComponent implements OnInit {
         console.error('Rating load error', error);
         this.loading = false;
       },
-    });
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
